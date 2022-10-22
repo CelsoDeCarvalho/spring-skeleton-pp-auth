f352635bacf9fa09467a07b5bb94d8d801285589
Celso De Carvalho <celsommade361@gmail.com>
2022-10-22 18:45:57 +0200

    created model User

---
+++ src/main/java/dev/mozcoder/usermanagementapi/UsermanagementapiApplication.java
@@ -0,0 +1,13 @@
+package dev.mozcoder.usermanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mozcoder/usermanagementapi/UsermanagementapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mozcoder.usermanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
